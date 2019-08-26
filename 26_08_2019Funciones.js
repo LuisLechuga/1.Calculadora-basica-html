aef758dc700b1d03ecb31ed43ba26ecc717b77f2
LuisLechuga <luislechuga1290@gmail.com>
2019-08-26 02:04:09 -0500

    1.Calculadorahtml

---
+++ 26_08_2019Funciones.js
@@ -0,0 +1,6 @@
+var suma = function(numero1,numero2){
+    var numero1 = parseFloat  (document.getElementById("numero1").value);
+    var numero2 = parseFloat  (document.getElementById("numero2").value);
+    var Resultado = numero1 + numero2;
+    return Resultado; 
+}
---
+++ if.js
@@ -0,0 +1,24 @@
+// let numero1 = '4'; //declarar una variable entera
+// let numero2 = '5' //variable tipo string
+
+// if (numero1 === numero2){
+//  console.log('son iguales')
+// }
+// else {
+//   console.log('son diferentes')
+// }
+
+let ambos = '8'
+// let l ;
+
+function Luis (l){
+  // console.log(ambos)
+  console.log(l)
+  Salomon();
+};
+function Salomon (){
+  console.log(ambos)
+};
+
+Luis(Salomon); //Lamar una funcion
+// Salomon();
